76f36612c3c84490945c5c38851754329fd555fe
sujith-26 <sujithg.22it@kongu.edu>
2025-06-12 00:02:30 +0530

    Fix Tailwind PostCSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
